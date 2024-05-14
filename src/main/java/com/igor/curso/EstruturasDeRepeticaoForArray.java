cc263e12d07ba95f792b7d709fcede224f320ebe
IgorPintoS <igorpintosantos05@gmail.com>
2024-05-13 21:08:33 -0300

    Quinto commit

--- src/main/java/com/igor/curso/EstruturasDeRepeticaoForArray.java
+++ src/main/java/com/igor/curso/EstruturasDeRepeticaoForArray.java
@@ -3,7 +3,7 @@ package com.igor.curso;
 public class EstruturasDeRepeticaoForArray {
     public static void main(String[] args) {
         String alunos [] = {"Felipe", "Jonas", "Julia", "Igor"};
-
+        System.out.println(alunos.length);
         for (int x = 0; x < alunos.length; x++){
             System.out.println("O aluno do indice " + x + " é " + alunos[x]);
         }
